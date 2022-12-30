7814900f31895f21bd08a429f70b63af97b9791d
Andrei Grusheuski <gruand13@gmail.com>
2022-12-30 19:27:18 +0300

    add client-server exchange (lesson4)

--- app/src/main.js
+++ app/src/main.js
@@ -1,16 +1,13 @@
 import $ from "jquery";
 
-$.get("../api", data=>{
-    data.forEach(file=>{
-        $("body").append(`<h1>${file}</h1>`);
-    })
-    
-},"JSON");
 
-$("button").click(()=>{
-    $.post("./api/createNewpage.php", {
-        "name" : $("input").val()
-    }), data=>{
-        console.log(data);
-    }
-});
+$.get("./api", data=>{
+    data.forEach(file =>{
+        $("body").append(`<h1>${file}</h1>`)
+    })
+
+}, "JSON")
+
+
+
+
